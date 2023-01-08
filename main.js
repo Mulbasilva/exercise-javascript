094f205df95c02a24fd0cda91e8747cfdcab3437
Mulba Joãozinho da Silva <dideus24@gmail.com>
2023-01-08 20:26:33 -0300

    segundo commit, treinando textContent e innerHTML

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-// let title1 = document.querySelector(".inicio__title-1");
-// let title2 = document.querySelector(".inicio__title-2");
+let title1 = document.querySelector(".inicio__title-1");
+let title2 = document.querySelector(".inicio__title-2");
+let title3 = document.querySelector(".inicio__title-3");
+let title4 = document.querySelector(".inicio__title-4");
+let title5 = document.querySelector(".inicio__title-5");
+let title6 = document.querySelector(".inicio__title-6")
 
-// title1.textContent = "O titulo alterado a partir do DOM";
+title1.textContent = "O titulo alterado a partir do DOM";
 
+title2.textContent = "O titulo alterado a partir do DOM";
 
-// title2.textContent = "O titulo alterado a partir do DOM";
+title3.textContent = "Conversando com DOM";
+
+title4.innerHTML = "<h1><em>Aperfeiçoando a comunicção com DOM</em></h1>";
+
+title5.innerHTML = "<p><strong>My profile photo</strong></p>";
+
+title6.innerHTML = "<p><em>J'aime programer</em></p>"
+title6.querySelector(".inicio__description-6");
