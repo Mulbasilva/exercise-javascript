b2deb3588024f55edfc5275963acfca7f74044a8
Mulba Joãozinho da Silva <dideus24@gmail.com>
2023-01-08 19:24:45 -0300

    primeiro commit da minha pagina de exercicios de javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+// let title1 = document.querySelector(".inicio__title-1");
+// let title2 = document.querySelector(".inicio__title-2");
+
+// title1.textContent = "O titulo alterado a partir do DOM";
+
+
+// title2.textContent = "O titulo alterado a partir do DOM";
