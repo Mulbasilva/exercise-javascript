5c066fb0a086b58c55944dfe284d7fbadb13b6c3
Mulba Joãozinho da Silva <dideus24@gmail.com>
2023-01-09 19:55:55 -0300

    exercicios de timeline / linha do tempo com html e css

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let title2 = document.querySelector(".inicio__title-2");
 let title3 = document.querySelector(".inicio__title-3");
 let title4 = document.querySelector(".inicio__title-4");
 let title5 = document.querySelector(".inicio__title-5");
-let title6 = document.querySelector(".inicio__title-6")
+let title6 = document.querySelector(".inicio__title-6");
 
 title1.textContent = "O titulo alterado a partir do DOM";
 
@@ -15,5 +15,5 @@ title4.innerHTML = "<h1><em>Aperfeiçoando a comunicção com DOM</em></h1>";
 
 title5.innerHTML = "<p><strong>My profile photo</strong></p>";
 
-title6.innerHTML = "<p><em>J'aime programer</em></p>"
+title6.innerHTML = "<p><em>J'aime programer</em></p>";
 title6.querySelector(".inicio__description-6");
